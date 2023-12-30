f697e96642f3624cd1073695c02c3fe1f2e5565c
Aditya Peddireddy <99650596+Adityachaitu@users.noreply.github.com>
2023-12-30 11:45:39 +0530

    Add files via upload

---
+++ Chapter4/4_b1.c
@@ -0,0 +1,14 @@
+// P.ADITYA - CH.EN.U4CYS21055
+#include<stdio.h>
+    
+int main(){
+    
+    int i = 4, z = 12 ;
+    if ( i = 5 || z > 50 )
+        printf ( "Dean of students affairs\n" ) ;
+    else
+        printf ( "Dosa\n" ) ;
+    return 0;
+}
+
+// prints "Dean of students affairs" as we are not giving "==" in if condition for 'i'.
---
+++ Chapter4/4_d2.c
@@ -0,0 +1,23 @@
+// P.ADITYA - CH.EN.U4CYS21055
+#include<stdio.h>
+    
+int main(){
+    
+    char inp;
+    printf("Enter a Character: ");
+    scanf("%c",&inp);
+
+    if(inp>=65 && inp<=90){
+        printf("%c is a Capital Letter.",inp);
+    }
+    else if(inp>=97 && inp<=122){
+        printf("%c is a Small Letter.",inp);
+    }
+    else if(inp>=48 && inp<=57){
+        printf("%c is a Number.",inp);
+    }
+    else {
+        printf("%c is a Special Character.",inp);
+    }
+    return 0;
+}
