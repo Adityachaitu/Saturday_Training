43f6c1187781419b09cf3a5b78184376918bce75
Aditya Peddireddy <99650596+Adityachaitu@users.noreply.github.com>
2023-12-30 11:33:55 +0530

    Add files via upload

---
+++ Chapter3/3_c8.c
@@ -0,0 +1,17 @@
+// P.ADITYA - CH.EN.U4CYS21055
+#include<stdio.h>
+    
+int main(){
+    
+    int inp;
+    printf("Enter a integer: ");
+    scanf("%d",&inp);
+
+    if(inp<0){
+        printf("\nThe Absolute Value is %d",-(inp));
+    }
+    else {
+        printf("\nThe Absolute Value is %d",inp);
+    }
+    return 0;
+}
